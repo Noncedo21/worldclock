339c894200afa003619bfbe1e104f5f3f3fa92fa
Noncedo21 <132228442+Noncedo21@users.noreply.github.com>
2024-10-22 21:39:26 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ let dbnDate = moment().format("MMMM Do YYYY");
 let dbnTime = moment().tz("Africa/Johannesburg");
 let formattedDbnTime = moment(dbnTime).format("HH:mm:ss");
 let currentDbn = document.querySelector("#ZA-time");
+currentDbn.innerHTML = formattedDbnTime;
