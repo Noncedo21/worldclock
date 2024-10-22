bb03c1f366d5562afbc632a4ae2148bfab035e16
Noncedo21 <132228442+Noncedo21@users.noreply.github.com>
2024-10-22 22:25:17 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function showDurban() {
   let dbnDate = moment().format("MMMM Do YYYY");
   let currentDate = document.querySelector("#ZA-date");
-  currentDate.innerHTML = dbnDate; // Add this to update the date in the DOM
+  currentDate.innerHTML = dbnDate; // Add this to update the date
 
   let dbnTime = moment().tz("Africa/Johannesburg").format("HH:mm:ss");
   let currentDbn = document.querySelector("#ZA-time");
-  currentDbn.innerHTML = dbnTime; // This will update the time correctly
+  currentDbn.innerHTML = dbnTime; // This will update the time eMzansi
 }
 setInterval(showDurban, 1000);
 function showLagos() {
@@ -15,6 +15,16 @@ function showLagos() {
 
   let LagosTime = moment().tz("Africa/Lagos").format("HH:mm:ss");
   let currentLag = document.querySelector("#NG-time");
-  currentLag.innerHTML = LagosTime; // This will update the time correctly
+  currentLag.innerHTML = LagosTime; // This will update the time in Nigeria
 }
 setInterval(showLagos, 1000);
+function showNairobi() {
+  let NaiDate = moment().format("MMMM Do YYYY");
+  let currentNaiDate = document.querySelector("#KE-date");
+  currentNaiDate.innerHTML = NaiDate; // Add this to update the date
+
+  let NaiTime = moment().tz("Africa/Nairobi").format("HH:mm:ss");
+  let currentNai = document.querySelector("#KE-time");
+  currentNai.innerHTML = NaiTime; // This will update the Kenyan time
+}
+setInterval(showNairobi, 1000);
